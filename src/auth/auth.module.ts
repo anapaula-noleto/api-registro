37a29fd179409d213af06b3ce91c45d5a48d01b5
Ana Paula Nolêto <apaulanazarenonoleto@gmail.com>
2022-10-19 15:52:14 -0300

    feat(auth): implement login validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import * as dotenv from "dotenv";
 import { UsersModule } from "src/users/users.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { LoginValidationMiddleware } from "./middlewares/login-validation.middleware.ts";
 import { JwtStrategy } from "./strategies/jwt.strategy";
 import { LocalStrategy } from "./strategies/local.strategy";
 dotenv.config();
@@ -21,4 +22,8 @@ dotenv.config();
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy]
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes("login");
+  }
+}
