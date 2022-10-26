f5b353f0e68b5dc94e778530ccd5bab00688edf3
Ana Paula Nolêto <apaulanazarenonoleto@gmail.com>
2022-10-26 14:34:05 -0300

    fix(seed): add command and change ormconfig

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,29 +1,25 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import * as dotenv from "dotenv";
 import { Registration } from "../registrations/entities/registration.entity";
 import { User } from "../users/entities/user.entity";
 import { RegistrationsSeeder } from "./registrations.seeder";
 import { SeedService } from "./seed.service";
 import { UsersSeeder } from "./users.seeder";
+dotenv.config();
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: "postgres",
-        host: configService.get("DB_HOST"),
-        port: +configService.get<number>("DB_PORT"),
-        username: configService.get("DB_USERNAME"),
-        password: configService.get("DB_PASSWORD"),
-        database: configService.get("DB_NAME"),
-        entities: [User, Registration],
-        synchronize: true,
-        logging: true
-      }),
-      inject: [ConfigService]
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: Number.parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: false,
+      logging: true,
+      entities: [__dirname + "/../**/*.entity{.ts,.js}"]
     }),
     TypeOrmModule.forFeature([Registration, User])
   ],
